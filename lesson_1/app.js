e173aaf3fe6dce6be966d47e230c83d287d2660b
parimal <parimalm4653@gmail.com>
2023-10-08 10:56:49 +0530

    [Lesson 1] vue js if-else, done !

--- lesson_1/app.js
+++ lesson_1/app.js
@@ -2,12 +2,16 @@ const app = Vue.createApp({
   data() {
     return {
       title: "This is the title fromt the vue js file",
-      age: 12
+      age: 12,
+      toShow: false
     }
   },
   methods: {
     changeStatement() {
       this.title = "Hello, statement is changed !"
+    },
+    toShowDiv() {
+      this.toShow = !this.toShow
     }
   }
 })
