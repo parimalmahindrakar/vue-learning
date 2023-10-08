cfd278bca48853298f4f07933400125ce22b1b89
parimal <parimalm4653@gmail.com>
2023-10-08 16:50:11 +0530

    [Lesson 3] lesson 3 commits : vue cli

---
+++ lessoon_3/vue-modal-project/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <input type="text" ref="name">
+    <button @click="handleClickEvent">Click me</button>
+    <div v-if="showModal">
+      <Modal :theme="theme" @close="toggleModal">
+        <h1>Parimal's Giveaway</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit eaque aut, esse adipisci nemo excepturi assumenda inventore facilis nesciunt molestias, perferendis quidem porro rem laborum minus neque. Consequuntur, explicabo aperiam?</p>
+
+
+        <template v-slot:links>
+          <a href="#">sign up</a> <br>
+          <a href="#">More info</a>
+        </template>
+      </Modal>
+    </div>
+    <br><br>
+    <button @click="toggleModal">Show Modal</button>
+    <!-- this is the data binding props -->
+  </div>
+</template>
+
+<script>
+
+import Modal from './components/Modal.vue'
+
+export default {
+  name: 'App', // name is give to the component as our wish.
+  components: {
+    Modal
+  }, // components which are nested inside this currnet component.
+  data() {
+    return {
+      title: "This is the nice one",
+      header: "This is the header",
+      content: "This is the content",
+      theme: "sales",
+      showModal: false
+    }
+  },
+  methods: {
+    handleClickEvent() {
+      console.log("Hello there !")
+      let ele = this.$refs.name
+      ele.focus()
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+a {
+  color: white;
+}
+</style>
---
+++ lessoon_3/vue-modal-project/src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="backdrop" @click.self="emitCloseEvent">
+    <div class="modal" :class="{ sale: theme === 'sales' }">
+      <slot></slot>
+      <!-- this is the default slot -->
+
+      <slot name="links"></slot>
+
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: ['theme'],
+  methods: {
+    emitCloseEvent() {
+      this.$emit("close")
+    }
+  }
+}
+
+</script>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+  color:wheat;
+}
+
+.backdrop {
+  top: 0;
+  position: fixed;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+.sale {
+  background: rgb(216, 59, 59);
+}
+
+</style>
